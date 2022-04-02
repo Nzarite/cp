192900a7771f015636e951b67d938e83e85d31b6
Akshit Singh Chauhan <aschauhan2325@gmail.com>
2022-04-02 18:35:09 +0530

    updated snippets

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,17 +1,57 @@
 #include <bits/stdc++.h>
+
 using namespace std;
+
 typedef long long ll;
-typedef unsigned long long ull;
-typedef long double lld;
-#define all(x) x.begin(), x.end()
-#define loop(i, k, n) for (ll(i) = (k); (i) <= (n); (i)++)
-#define loopr(i, k, n) for (ll(i) = (k); (i) >= (n); (i)--)
+#define foi(n) for(ll i=0; i<(n); i++)
+#define loop(i, k, n) for(ll (i)=(k); (i)<=(n); (i)++)
+#define loopr(i, k, n) for(ll (i)=(k); (i)>=(n); (i)--)
+#define input(v) for(auto &inv:v) cin>>inv
+#define output(v) for(auto &prv:v) cout<<prv<<" "; cout<<'\n'
+#define lb lower_bound
+#define ub upper_bound
+#define ins insert
+#define setbits(x) __builtin_popcountll(x)
+#define zrobits(x) __builtin_ctzll(x)
+#define ps(x,y) fixed<<setprecision(y)<<x
+#define nline "\n"
+#define pb push_back
+#define ppb pop_back
 #define ff first
 #define ss second
-#define input(a) for (auto &i : a)cin >> i;
-#define output(a) for (auto &i : a)cout << i << ' ';cout << '\n';
-#define py cout << "YES\n"
-#define pn cout << "NO\n"
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define minv(a) *min_element(all(a))
+#define maxv(a) *max_element(all(a))
+#define py cout<<"YES\n"
+#define pn cout<<"NO\n"
+ 
+typedef unsigned long long ull;
+typedef long double lld;
+typedef pair<int, int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<ll>> vvl;
+typedef vector<string> vstr;
+typedef set<int> seti;
+typedef set<ll> setl;
+typedef multiset<int> mseti;
+typedef map<int, int> mii;
+typedef map<ll, ll> mll;
+ 
+ll gcd(ll a, ll b) {if (b > a) {return gcd(b, a);} if (b == 0) {return a;} return gcd(b, a % b);}
+ll expo(ll a, ll b, ll m) {ll res = 1; while (b > 0) {if (b & 1)res = (res * a) % m; a = (a * a) % m; b = b >> 1;} return res;}
+vector<ll> primeFactors(ll n) {vector<ll> v; while(!(n&1)){v.push_back(2);n>>=1;} for(ll i=3; i*i <= n; i+=2){while(n % i == 0) {v.push_back(i); n=n/i;}} if(n>2) v.push_back(n); return v;} 
+vector<ll> sieve(int n) {int*arr = new int[n + 1](); vector<ll> vect; for (int i = 2; i <= n; i++)if (arr[i] == 0) {vect.push_back(i); for (int j = 2 * i; j <= n; j += i)arr[j] = 1;} return vect;}
+bool isPrime(ll n) {if (n<=1) return false; for (ll i=2; i*i<=n; i++) {if (n%i==0) return false;} return true;}
+ll mod_add(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a + b) % m) + m) % m;}
+ll mod_mul(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a * b) % m) + m) % m;}
+ll mod_sub(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a - b) % m) + m) % m;}
+ 
 #ifndef ONLINE_JUDGE
 #define dbg(x) cerr << #x <<" "; _print(x); cerr << endl;
 #else
@@ -47,7 +87,7 @@ int main()
 #endif
 
     ll test_case = 1;
-    cin >> test_case;
+    // cin >> test_case;
     while (test_case--)
     {
         solve();
@@ -56,5 +96,5 @@ int main()
 
 void solve()
 {
-    cout << "Hello World";
+    cout << "hello world\n";
 }
