4aa7b791c4da60c41d6bcea084849a9ffcdade4a
Ayush Chauhan <ayoushchauhan1996@gmail.com>
2022-10-17 13:45:21 +0530

    Stein’s Algorithm for finding GCD

---
+++ math/Steins_Algorithm_for_finding_GCD.cpp
@@ -0,0 +1,5 @@
+/*
+Stein’s algorithm or binary GCD algorithm is an algorithm that computes the greatest common divisor 
+of two non-negative integers. Stein’s algorithm replaces division with arithmetic shifts,
+comparisons, and subtraction.
+*/
